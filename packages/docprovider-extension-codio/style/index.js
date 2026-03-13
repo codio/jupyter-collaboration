55ba79a9363d28a51f0cdc70a1917588fd798251
Andrey Nikitin <anikitin@codio.com>
2026-03-13 14:43:01 +0000

    Prepare repos before full fork.

--- packages/docprovider-extension-codio/style/index.js
+++ packages/docprovider-extension-codio/style/index.js
@@ -3,4 +3,4 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
-import '@jupyter/collaboration/style/index.js';
+import '@codioedu/jupyter-collaboration-codio/style/index.js';
